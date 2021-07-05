be9a0a2b96ead31c836781c1d44d9bdac29cb8ca
hiroki_sadahisa <sadahisa@kairos.co.jp>
2021-07-05 19:10:12 +0900

    Update: カテゴリー追加

--- nuxt.config.js
+++ nuxt.config.js
@@ -1,3 +1,5 @@
+import axios from 'axios';
+
 export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'universal',
@@ -45,5 +47,65 @@ export default {
 
   // Build Configuration: https://go.nuxtjs.dev/config-build
   build: {
-  }
+  },
+
+  router: {
+    extendRoutes(routes, resolve) {
+      routes.push({
+        path: '/page/:p',
+        component: resolve(__dirname, 'pages/index.vue'),
+        name: 'page',
+      });
+      routes.push({
+        path: '/category/:categoryId/page/:p',
+        component: resolve(__dirname, 'pages/index.vue'),
+        name: 'category',
+      });
+    },
+  },
+
+  generate: {
+    async routes() {
+      const limit = 3
+      const range = (start, end) =>
+        [...Array(end - start + 1)].map((_, i) => start + i)
+
+      // 一覧のページング
+      const pages = await axios
+        .get(`https://jam-jam2-1.microcms.io/api/v1/blog?limit=0`, {
+          headers: { 'X-API-KEY': '865c366d-79c3-4727-b68f-a6f22ee0876a' },
+        })
+          .then((res) =>
+            range(1, Math.ceil(res.data.totalCount / limit)).map((p) => ({
+              route: `/page/${p}`,
+            }))
+          )
+
+      const categories = await axios
+        .get(`https://jam-jam2-1.microcms.io/api/v1/categories?fields=id`, {
+          headers: { 'X-API-KEY': '865c366d-79c3-4727-b68f-a6f22ee0876a' },
+        })
+          .then(({ data }) => {
+            return data.contents.map((content) => content.id)
+          });
+
+      // カテゴリーページのページング
+      const categoryPages = await Promise.all(
+        categories.map((category) =>
+          axios.get(
+            `https://jam-jam2-1.microcms.io/api/v1/blog?limit=0&filters=category[equals]${category}`,
+            { headers: { 'X-API-KEY': '865c366d-79c3-4727-b68f-a6f22ee0876a' } }
+          )
+            .then((res) =>
+              range(1, Math.ceil(res.data.totalCount / 10)).map((p) => ({
+                route: `/category/${category}/page/${p}`,
+              })))
+        )
+      )
+
+      // 2次元配列になってるのでフラットにする
+      const flattenCategoryPages = [].concat.apply([], categoryPages)
+      return [...pages, ...flattenCategoryPages]
+    },
+  },
 }
